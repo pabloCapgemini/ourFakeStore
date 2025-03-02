33f48a004d9508c30dbfa0de889799b2f7d02da0
Pablo Ugaz <pablo.ugaz@capgemini.com>
2025-03-02 16:48:44 -0600

    removed playwright sample test

--- tests/storeUX.spec.ts
+++ tests/storeUX.spec.ts
@@ -1,13 +1,9 @@
 import { _electron as electron, test, expect } from '@playwright/test';
 
 test('should launch Electron app', async () => {
-    // Launch the Electron app
     const app = await electron.launch({ args: ['.webpack/arm64/main/'] });
 
-    // Get the first window
     const window = await app.firstWindow();
-
-    // Verify the title of the app
     const title = await window.title();
     expect(title).toBe('Fake Store');
 

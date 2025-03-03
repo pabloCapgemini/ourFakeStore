bb0e13adf83f92e77666c44aab1bbae4760c6a15
Pablo Ugaz <pablo.ugaz@capgemini.com>
2025-03-03 15:34:06 -0600

    remove old load product logic in renderer

--- src/renderer.ts
+++ src/renderer.ts
@@ -11,23 +11,6 @@ document.body.appendChild(app);
 
 const productList = document.getElementById("product-list");
 
-const loadProducts = async () => {
-  const products = await window.storeAPI.fetchProducts();
-  if (products instanceof Error) {
-    productList!.innerHTML = "<li>Failed to fetch products</li>";
-    return;
-  } else {
-    const loadedProducts = products as Product[];
-    if (products.length > 0) {
-      productList!.innerHTML = loadedProducts
-        .map((product) => `<li>${product.title} - $${product.price}</li>`)
-        .join("");
-    } else {
-      productList!.innerHTML = "<li>No products available. Please come back soon!</li>";
-    }
-  }
-
-};
 
 window.storeAPI.loadProducts((event, productsOrError) => {
   console.log(`loading products: ${productsOrError}`);
@@ -45,5 +28,4 @@ window.storeAPI.loadProducts((event, productsOrError) => {
     }
   }
 });
-// loadProducts();
 

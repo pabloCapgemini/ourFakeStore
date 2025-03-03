cf3a6fe10b38cc4b6844435eae4e822b1970e51c
Pablo Ugaz <pablo.ugaz@capgemini.com>
2025-03-03 17:49:28 -0600

    removed test StoreRepos, we now only have the Prod version

--- src/StoreRepository.ts
+++ src/StoreRepository.ts
@@ -16,16 +16,3 @@ export class StoreRepository {
         }
     }
 }
-
-export class EmptyStoreRepository extends StoreRepository {
-    async getProductsOrError(): Promise<Product[] | ApiError> {
-        return [];
-    }
-}
-
-export class ErrorStoreRepository extends StoreRepository {
-
-    async getProductsOrError(): Promise<Product[] | ApiError> {
-        return new ApiError("Failed to fetch products");
-    }
-}

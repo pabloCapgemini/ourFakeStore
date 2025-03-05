70fea846d811c0b365bb27784ef4c068df22194a
Pablo Ugaz <pablo.ugaz@capgemini.com>
2025-03-05 08:43:47 -0600

    added test of error message when loaing products

--- src/index.ts
+++ src/index.ts
@@ -50,10 +50,18 @@ app.on('activate', () => {
   }
 });
 
+let isErrorOnLoadingProducts = false;
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
 ipcMain.handle("fetch-products", async () => {
+  if (isErrorOnLoadingProducts) {
+    return [];
+  }
   const products = await getProducts();
   console.log('products: ' + products);
   return products;
+});
+
+ipcMain.on('set-error-loading-products', (event) => {
+  isErrorOnLoadingProducts = true;
 });
